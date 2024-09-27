acafdce84778d75b1f1e9663b491957b3806c789
Nikolai Ng <165669417+Niko2xni@users.noreply.github.com>
2024-09-27 19:40:45 +0800

    Update

--- login.js
+++
@@ -1,8 +0,0 @@
-const loginForm = document.getElementById('login');
-
-document.getElementById('login').addEventListener('submit', function (event) {
-    event.preventDefault();
-    alert('Login Successfully Submitted!');
-    window.location.href = "menu.html";
-});
-
